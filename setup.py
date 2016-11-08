87197c04c10b7dd680ba64ead3a39369a45845e3
Brock Anderson <brock@bandersgeo.ca>
2016-11-07 20:56:43 -0800

    added a plugin which injects a swagger-ui API console into the dataset (package) view.  this is a preliminary commit -- the swagger console displayed is just an example, and not yet linked to a swagger file url stored in BCDC.

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
     entry_points='''
         [ckan.plugins]
         # Add plugins here, e.g.
-        # myplugin=ckanext.swaggerconsole.plugin:PluginClass
+        swagger_console=ckanext.swaggerconsole.plugin:SwaggerConsolePlugin
     ''',
 )
